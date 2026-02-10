a206554820ebcde9861018f9de74baeded9bf106
Gripsi0810 <sindwanigripsi@gmail.com>
2026-02-10 11:58:50 +0530

    fix port for render

--- index.js
+++ index.js
@@ -92,7 +92,8 @@ app.post("/bfhl", async (req,res)=>{
         });
     }
 });
-app.listen(3000, ()=>{
-    console.log("Server running");
-});
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, ()=>{
+    console.log("Server running on port " + PORT);
+});
